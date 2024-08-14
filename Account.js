116af62f704e7a5dafb2f6e6690214e91ad99d96
Evilen Barreto <159492801+evbarretoo@users.noreply.github.com>
2024-08-14 11:29:06 +0000

    Update repository

---
+++ Account.js
@@ -0,0 +1,30 @@
+class Account {
+    // validads
+    #password
+    #balance
+
+    constructor(user) {
+        this.email = user.email
+        this.#password = user.password 
+        this.#balance = 0
+    }
+
+    getBalance(email, password) {
+        if (this.email === email && this.#password === password) {
+            return this.#balance
+        } else {
+            return null
+        }
+    }
+}
+
+// objeto
+const user = {
+    email: 'johndogarfeld@gmail.com',
+    password: '2706'
+}
+
+// instancia
+const myAccount = new Account(user)
+
+console.log(myAccount)
